af1fe98f338bb32cffa916c37a972a174291cb14
Rahul Gupta <rahul@growingdata.com.au>
2018-05-12 21:48:27 +1000

    Residual Analysis for ARIMA and function to seperate files

---
+++ src/sort.score.R
@@ -0,0 +1,9 @@
+sort.score <- function(x, score = c("bic", "aic")){
+  if (score == "aic"){
+    x[with(x, order(AIC)),]
+  } else if (score == "bic") {
+    x[with(x, order(BIC)),]
+  } else {
+    warning('score = "x" only accepts valid arguments ("aic","bic")')
+  }
+}
